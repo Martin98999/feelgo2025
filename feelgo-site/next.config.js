6a1c37f2da2af2f225611e06ba34139920484331
Martin98999 <audiojakes@gmail.com>
2025-05-25 13:52:18 +0200

    Update next.config.js

--- feelgo-site/next.config.js
+++ feelgo-site/next.config.js
@@ -4,4 +4,17 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig{
+  "name": "feelgo-nextjs",
+  "version": "1.0.0",
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "13.4.12",
+    "react": "18.2.0",
+    "react-dom": "18.2.0"
+  }
+}
